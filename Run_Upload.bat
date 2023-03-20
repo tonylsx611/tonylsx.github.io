9d4ed792dec55c038fa8b9626f25820b8b1ed06f
tonylsx <tonylsx611@gmail.com>
2023-03-20 22:41:54 +0800

    2023-03-20 updated

--- Run_Upload.bat
+++ Run_Upload.bat
@@ -1,8 +1,10 @@
 @echo off
 git add .
 set date=%date:~0,4%-%date:~5,2%-%date:~8,2%
-echo Adding files...  Time: %date%\n
+echo Adding files...  Time: %date%
+echo.
 git commit -m "%date% updated"
 git push origin hexo
+echo.
 echo Push to Github Successfully!\n
 @pause
